b9d6e377da27347da39b2ea5e31f80e1a909c9af
GiuseppeLiGioi <pepe2117k@gmail.com>
2025-01-14 12:22:53 +0100

    ADD: start the first exercise

--- main.js
+++ main.js
@@ -9,3 +9,22 @@ Si può fare? Certo che si basta ragionare un po’.
 Nota:
 Non è necessario provvedere alla validazione delle email
 */
+
+let arrayInvitati = ["pepe@gmail.com" , "sara@gmail.com" , "drevi@gmail.com" , "cristian@gmail.com" , "edoardo@gmail.com"]; //creazione del mio array degli invitati
+let isEmailValid = false;
+let mailUtente = console.log(prompt("inserisci la tua e-mail")); //chiedo all'utente di inserire la sua e-mail.
+
+//a questo punto facciamo un ciclo for, dove al suo interno mettiamo la condizione per capire se la sua e-mail è valida: TRUE. Oppure non valida: FALSE.
+for(let i=0; i<arrayInvitati.length; i++){
+    if(arrayInvitati[i]===mailUtente){
+        isEmailValid=true;
+
+    }
+    else{
+        isEmailValid=false;
+
+    }
+}
+console.log(isEmailValid);
+
+
